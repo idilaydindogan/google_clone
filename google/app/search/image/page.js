8e05b468f351ecce07a4c3896e1dac3b95244642
Idil Dogan <idilaydindogan@gmail.com>
2023-03-23 17:18:26 +0300

    Search pages and layout added

---
+++ google/app/search/image/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImageSearchPage = () => {
+	return <div>page</div>;
+};
+
+export default ImageSearchPage;
---
+++ google/app/search/web/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WebSearcgPage = () => {
+	return <div>WebSearcgPage</div>;
+};
+
+export default WebSearcgPage;
