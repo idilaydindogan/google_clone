5c0d2cdab1d62dd00c2daf4fec6fdeba54006eb0
Idil Dogan <idilaydindogan@gmail.com>
2023-03-23 16:44:30 +0300

    Footer comp & Location added

---
+++ google/components/CountryLookUp.js
@@ -0,0 +1,18 @@
+"use client";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+const CountryLookUp = () => {
+	const [country, setCountry] = useState("United States");
+	useEffect(() => {
+		fetch(
+			`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+		)
+			.then((res) => res.json())
+			.then((data) => setCountry(data.country));
+	}, []);
+
+	return <div>{country}</div>;
+};
+
+export default CountryLookUp;
---
+++ google/components/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+import CountryLookUp from "./CountryLookUp";
+import Link from "next/link";
+
+const Footer = () => {
+	return (
+		<footer className="absolute bottom-0 text-sm text-gray-700 bg-[#f2f2f2] w-full">
+			<div className="border-b px-12 py-3 ">
+				<CountryLookUp />
+			</div>
+			<div className="flex flex-col sm:flex-row sm:justify-around items-center py-3 space-y-5">
+				<div className="space-x-6 items-center">
+					<Link href={"/"} className="link">
+						About
+					</Link>
+					<Link href={"/"} className="link">
+						Advertising
+					</Link>
+					<Link href={"/"} className="link">
+						Business
+					</Link>
+					<Link href={"/"} className="link">
+						How Search Works
+					</Link>
+				</div>
+				<div className="space-x-6 items-center">
+					<Link href={"/"} className="link">
+						Privacy
+					</Link>
+					<Link href={"/"} className="link">
+						Terms
+					</Link>
+					<Link href={"/"} className="link">
+						Settings
+					</Link>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
