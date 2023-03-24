76cd4fcc5ef44bc9141966d4cb698fc574b0c7da
Idil Dogan <idilaydindogan@gmail.com>
2023-03-24 16:23:04 +0300

    Fetch data from google api

--- google/app/search/web/page.js
+++ google/app/search/web/page.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const WebSearcgPage = () => {
-	return <div>WebSearcgPage</div>;
+const WebSearcgPage = async ({ searchParams }) => {
+	const response = await fetch(
+		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+	);
+	const data = await response.json();
+	console.log(data);
+	const results = data.items;
+
+	return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 };
 
 export default WebSearcgPage;
